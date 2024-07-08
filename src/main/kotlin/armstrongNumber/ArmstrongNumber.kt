1fd7952f2a24cd259196462484c6eee68da164fc
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-07-08 22:58:57 +0200

    Switch to a custom `pow` function

--- src/main/kotlin/armstrongNumber/ArmstrongNumber.kt
+++ src/main/kotlin/armstrongNumber/ArmstrongNumber.kt
@@ -1,14 +1,14 @@
 package armstrongNumber
 
-import kotlin.math.pow
-
 object ArmstrongNumber {
     fun check(input: Int): Boolean {
         val numString = input.toString()
         val numLength = numString.length
 
         return numString
-            .map { it.digitToInt().toDouble().pow(numLength) }
-            .sum().toInt() == input
+            .map { it.digitToInt() pow numLength }
+            .sum() == input
     }
+
+    private infix fun Int.pow(n: Int): Int = if (n == 0) 1 else this * pow(n - 1)
 }
