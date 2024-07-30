c37d729e16bdcc0b2bda5b5522eb5799b2508501
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-07-30 18:00:32 +0200

    Use `atStartOfDay()`

--- src/main/kotlin/gigasecond/Gigasecond.kt
+++ src/main/kotlin/gigasecond/Gigasecond.kt
@@ -4,7 +4,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 class Gigasecond(today: LocalDateTime) {
-    constructor(today: LocalDate) : this(today.atTime(0, 0))
+    constructor(today: LocalDate) : this(today.atStartOfDay())
 
     val date: LocalDateTime = today.plusSeconds(1_000_000_000)
 }
